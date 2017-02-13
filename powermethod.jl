97fb8768aefbb0edf1c28497701ed18048abf1cc
Caitlin Kennedy <kenned67@purdue.edu>
2017-02-13 11:30:49 -0500

    converges

--- powermethod.jl
+++ powermethod.jl
@@ -1,47 +1,37 @@
 function generateSparceMatrix(rows, columns, density)
   sparcematrix = sprand(rows, columns, density)
   fullmatrix = full(sparcematrix)
-    @show fullmatrix #sparcematrix
   return fullmatrix
 end
 
-function powerMethod(fullMatrix, iteration)
-    x0 = ones(size(fullMatrix, 1) , 1)
-    power = fullMatrix * x0
-    for i=1:(iteration - 1)
-        #@show power #sparse(power)
-        power = fullMatrix * power
-        power = power / norm(power)
-    end
-    return power
-  #   if length(find(power)) < 1
-  #       print()
-  #   else
-  #       findnz(abs(power))
-  #       scale = minimum(findnz(abs(power))[3])
-  #     if findfirst(power, scale) == 0
-  #         scale = scale * -1
-  #     end
-  #     #doesn't work if there is a zero value
-  #     dominantEigenvector = power / scale
-  #     return dominantEigenvector
-  #  end
-end
+function powerMethod(matrix)
+    x0 = ones(size(matrix, 1) , 1)
+    power = x0/ norm(x0)
 
-function normalizedEig(eigvec)
-    eigvec
-  for i = 1:size(eigvec, 2)
-      #replace with a non zero value
-      domEig = sparsevec(eigvec[1:end, i])
-      findnz(domEig)
-      min = minimum(findnz(abs(domEig))[2])
-      if findfirst(domEig, min) == 0
-          min = min * -1
+    for i=1:(10000)
+      y = matrix * power
+
+      lam = power' * y
+      newPower = y / norm(y)
+      normr = norm(y - newPower * lam)
+      if normr/lam[1] < 1.0e-8 #1.0e-8
+        @show i
+
+        return newPower #dot(newPower, power)
       end
-      eigvec[1:end, i] = eigvec[1:end, i] / min
-
+      power = newPower
+    end
+    @printf("Doesnt converge quickly enough\n")
+    return [0]
   end
-    return sparse(eigvec)
+
+function normalizedEig(matrix)
+    @show values, vectors = eigs(matrix)
+    @printf("\nHI\n")
+  @show   max = maximum(values)
+    index = findfirst(values, max)
+    return vectors[index,:]
+
 end
 
 function inefficientPower(fullMat)
@@ -50,13 +40,16 @@ function inefficientPower(fullMat)
     return x/ minimum(findnz(abs(x))[3])
 end
 
-fullMat = generateSparceMatrix(100, 100, .01)
-fullMat = 1000.*fullMat
+fullMat = generateSparceMatrix(1000, 1000, .0009)
+#fullMat = 1000.*fullMat
 #fullMat = [2 -12; 1 -5]
- dominantEigenvector = powerMethod(fullMat, 50 ) #sparse(powerMethod(fullMat, 5 ))
+ dominantEigenvector = powerMethod(sparse(fullMat)) #sparse(powerMethod(fullMat, 5 ))
  @show sparse(dominantEigenvector)
 #@show inefficientPower(fullMat)
 println("\n\n")
-allEigenvectors = eigvecs(fullMat)
-@show sparse(allEigenvectors)
-#@show normalizedEig(eigvecs(fullMat))
+if dominantEigenvector != [0]
+  allEigenvectors = eigvecs(fullMat)
+ @show sparse(allEigenvectors)
+#  @show normalizedEig(fullMat)
+
+end
