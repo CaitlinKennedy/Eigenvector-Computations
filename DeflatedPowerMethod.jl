f4defbdf1e314762c77234e16aee5862a7b4943a
Caitlin Kennedy <kenned67@purdue.edu>
2018-05-07 16:56:52 -0400

    updated version

--- DeflatedPowerMethod.jl
+++ DeflatedPowerMethod.jl
@@ -1,41 +1,51 @@
-function powerMethod(matrix)
+
+function powerMethod(matrix, mymatvec)
     x0 = ones(size(matrix, 1) , 1)
     power = x0 #/ norm(x0)
 
     for i=1:(10000)
       y = matrix * power
-
+      mymatvec += 1
       lam = power' * y
       normr = norm(y - power * lam)
-      if normr/lam[1] < 1.0e-8
+      if normr/abs(lam[1]) < 1.0e-8
         #do something to run again.
-        return sparse(y/ norm(y)), dot((matrix * y), y)/dot(y,y)
+        mymatvec += 1
+        @show lam[1], normr
+        return sparse(y/ norm(y)), dot((matrix * y), y)/dot(y,y), mymatvec
       end
       power = y / norm(y)
     end
     @printf("Doesnt converge quickly enough\n")
-    return [0], norm(power)
+    return [0], norm(power), mymatvec
   end
-
-n = 1000
+ #A + At
+mymatvec = 0
+n = 25
 matrix = sprand(n, n, .05)
 matrix = matrix + matrix'
-dominantEigenvector, dominantEigenvalue = powerMethod(matrix)
+dominantEigenvector, dominantEigenvalue, mymatvec = powerMethod(matrix, mymatvec)
 lam,v = eigs(matrix;nev=3)[1:2] # returns the largest 2 eigenvalues lambda vectors v
-
+a, b, c, d, matvec, e = eigs(matrix; nev=2)
 if length(dominantEigenvector) > 1
   x = -dominantEigenvector
   x[1] += 1.0
   Q = eye(n) - 2*x*x'/(x'*x)[1]
   B = (Q*matrix*Q')[2:end,2:end]
-  eigVecTwo, eigValTwo = powerMethod(B)
+  eigVecTwo, eigValTwo, mymatvec = powerMethod(B, mymatvec)
   lamB, vB = eigs(B;nev=2)[1:2] # returns the largest 2 eigenvalues lambda vectors v
   eigVecTwo = Q*[0.0; eigVecTwo]
+  @show eigVecTwo
+  @show v[:,2]
   @show abs(lam[1]-(dominantEigenvector'*matrix*dominantEigenvector)[1])
   @show 1-abs(v[:, 1]'*dominantEigenvector) # should be close to 0
 
   print("\n\n")
-  @show abs(lam[2]-(eigVecTwo'*matrix*eigVecTwo)[1])
-  @show 1-abs(v[:, 2]'*eigVecTwo) # should be close to 0
+  #@show abs(lam[2]-(eigVecTwo'*matrix*eigVecTwo)[1])
+  #@show 1-abs(v[:, 2]'*eigVecTwo) # should be close to 0
+  print("\n EIGS MATVEC = ", matvec)
+  print("\n MY MATVEC = ", mymatvec, "\n")
 
+  #@show (lam)
+  #@show (lamB)
 end
--- powermethod.jl
+++ powermethod.jl
@@ -1,6 +1,6 @@
 function powerMethod(matrix)
     x0 = ones(size(matrix, 1) , 1)
-    power = x0 #/ norm(x0)
+    power = x0 / norm(x0)
 
     for i=1:(10000)
       y = matrix * power
@@ -10,12 +10,12 @@ function powerMethod(matrix)
       normr = norm(y - power * lam )
       if normr/lam[1] < 1.0e-8
         @show i
-        return sparse(newPower)
+        return newPower
       end
       power = newPower
     end
     @printf("Doesnt converge quickly enough\n")
-    return [0]
+    return zeros(0)
   end
 
 #function inefficientPower(fullMat)
@@ -24,12 +24,18 @@ function powerMethod(matrix)
 #    return x/ minimum(findnz(abs(x))[3])
 #end
 
-matrix = sprand(1000, 1000, .0005)
+matrix = sprand(1000, 1000, .005) #bigger thqn 3log(n)x/2
+matrix = matrix + matrix'
 dominantEigenvector = powerMethod(matrix)
-@show dominantEigenvector
+dominantEigenvector
 #@show inefficientPower(fullMat)
 println("\n\n")
-if dominantEigenvector != [0]
-  allEigenvectors = eigvecs(full(matrix))
-  @show sparse(allEigenvectors)
+if length(dominantEigenvector) == 0
+  lam,v = eigs(matrix;nev=2)[1:2] # returns the largest eigenvalue lambda vector v
+  @show lam
+  @show abs(lam)
+else
+  lam,v = eigs(matrix;nev=1)[1:2] # returns the largest eigenvalue lambda vector v
+  @show abs(lam-(dominantEigenvector'*matrix*dominantEigenvector)[1])
+  @show 1-abs(v'*dominantEigenvector) # should be close to 0
 end
